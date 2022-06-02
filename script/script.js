11a968aa42c92cca49e50d586056da1c410cd73a
cel8 <alex.celotti@gmail.com>
2022-06-02 09:03:41 -0400

    Prevent input drag and selection

--- script/script.js
+++ script/script.js
@@ -2,3 +2,8 @@ const img = document.querySelectorAll('img')
 img.forEach(i => {
   i.setAttribute("draggable", false);
 });
+
+const input = document.querySelectorAll('input')
+input.forEach(i => {
+  i.setAttribute("draggable", false);
+});
